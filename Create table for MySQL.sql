2465ab8daa95c8b7a867eaa58c7d7160e4462bd5
asrbacr <gek2372@ya.ru>
2023-11-03 12:44:02 +0300

    Update Create table for MySQL.sql
    add date

--- Create table for MySQL.sql
+++ Create table for MySQL.sql
@@ -57,3 +57,23 @@ CREATE TABLE animals (
     FOREIGN KEY (type_id) REFERENCES type_animals (id),
     FOREIGN KEY (group_id) REFERENCES group_animals (id)
 );
+
+-- Заполнениен таблицы данными
+INSERT INTO animals (name, type_id, birth_date, group_id)
+VALUES 
+    ('Fido', 1, '2020-01-01', 1),
+    ('Whiskers', 2, '2019-05-15', 1),
+    ('Hammy', 3, '2021-03-10', 1),
+    ('Buddy', 1, '2018-12-10', 1),
+    ('Smudge', 2, '2020-02-20', 1),
+    ('Peanut', 3, '2021-08-01', 1),
+    ('Bella', 1, '2019-11-11', 1),
+    ('Oliver', 2, '2020-06-30', 1),
+    ('Thunder', 4, '2015-07-21', 2), 
+    ('Sandy', 5, '2016-11-03', 2), 
+    ('Eeyore', 6, '2017-09-18', 2), 
+    ('Storm', 4, '2014-05-05', 2), 
+    ('Dune', 5, '2018-12-12', 2), 
+    ('Burro', 6, '2019-01-23', 2), 
+    ('Blaze', 4, '2016-02-29', 2), 
+    ('Sahara', 5, '2015-08-14', 2);
